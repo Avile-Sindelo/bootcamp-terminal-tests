1a4b0f9572345ea67a503c469d49e09f0d36dcb4
Avile Sindelo <avilesindelo@gmail.com>
2023-07-26 16:19:16 +0200

    fixing the github actions

---
+++ test/findItemsOver.test.js
@@ -0,0 +1,44 @@
+import assert from "assert";
+import findItemsOver from "../findItemsOver.js";
+
+describe('Testing the findItemsOver function on the terminal', function(){
+    it('should test if the function returns only the items whose quantity is over the specified threshold', function(){
+        var itemList = [
+            {name : 'apples', qty : 10},
+            {name : 'pears', qty : 37},
+            {name : 'bananas', qty : 27},
+            {name : 'apples', qty : 3},
+        ];
+        
+        var results = [
+            {name : 'pears', qty : 37},
+            {name : 'bananas', qty : 27},
+        ];
+        
+        var itemList2 = [
+            {name : 'apples', qty : 10},
+            {name : 'pears', qty : 19},
+            {name : 'bananas', qty : 17},
+            {name : 'apples', qty : 3},
+        ];
+        
+        var results2 = [];
+        
+        var itemList3 = [
+            {name : 'apples', qty : 40},
+            {name : 'pears', qty : 20},
+            {name : 'bananas', qty : 23},
+            {name : 'apples', qty : 37}
+        ];
+        
+        var results3 = [
+            {name : 'apples', qty : 40},
+            {name : 'bananas', qty : 23},
+            {name : 'apples', qty : 37}
+        ];
+        
+        assert.deepEqual(results, findItemsOver(itemList, 20));
+        assert.deepEqual(results2, findItemsOver(itemList2, 20));
+        assert.deepEqual(results3, findItemsOver(itemList3, 20));
+    });
+});
