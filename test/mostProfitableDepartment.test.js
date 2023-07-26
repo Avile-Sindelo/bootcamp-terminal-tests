3397ae3c774faa1a3157a64c4c66b6e195cc6557
Avile Sindelo <avilesindelo@gmail.com>
2023-07-26 16:39:59 +0200

    fixed the broken test

--- test/mostProfitableDepartment.test.js
+++ test/mostProfitableDepartment.test.js
@@ -21,31 +21,6 @@ describe('Testing the Most Profitable Department function on the terminal', func
             {department : 'carpentry', sales : 16109, day : 'Friday'},
         ];
         
-        var salesData2 = [
-            {department : 'electronics', sales : 4500, day : 'Monday'},
-            {department : 'outdoor', sales : 1500, day : 'Monday'},
-            {department : 'carpentry', sales : 5500, day : 'Monday'},
-            {department : 'steelwork', sales : 7500, day : 'Tuesday'},
-            {department : 'outdoor', sales : 2505, day : 'Tuesday'},
-            {department : 'carpentry', sales : 1540, day : 'Tuesday'},
-            {department : 'hardware', sales : 1500, day : 'Wednesday'},
-            {department : 'outdoor', sales : 8507, day : 'Wednesday'},
-            {department : 'carpentry', sales : 8009, day : 'Wednesday'},
-            {department : 'hardware', sales : 12000, day : 'Thursday'},
-            {department : 'carpentry', sales : 6109, day : 'Thursday'},
-            {department : 'hardware', sales : 7005, day : 'Friday'},
-            {department : 'electronics', sales : 12006, day : 'Friday'},
-            {department : 'electronics', sales : 16109, day : 'Saturday'},
-            {department : 'steelwork', sales : 7500, day : 'Tuesday'},
-            {department : 'outdoor', sales : 2505, day : 'Tuesday'},
-            {department : 'carpentry', sales : 1540, day : 'Tuesday'},
-            {department : 'steelwork', sales : 1500, day : 'Wednesday'},
-            {department : 'carpentry', sales : 8009, day : 'Wednesday'},
-        ];
-        
-        assert.equal('outdoor', mostProfitableDepartment(salesData), "Most profitable department is 'outdoor' for dataset 1");
-       
+        assert.equal("Most profitable department is 'outdoor' for dataset 1", mostProfitableDepartment(salesData));
     });
-
-    
 });
